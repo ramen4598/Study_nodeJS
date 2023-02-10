612ebcd8a1b82dd991d83f4028464379beba2a36
tired_i <ramen4598@naver.com>
2023-02-11 00:26:31 +0900

    23.02.11 commit - make a function named templateHTML for refactoring.

--- main.js
+++ main.js
@@ -2,6 +2,35 @@ let http = require("http");
 let fs = require("fs");
 let url = require("url");
 
+function templateHTML(title, list, description){
+  return `
+   <!DOCTYPE html>
+   <html lang="en">
+     <head>
+       <meta charset="UTF-8" />
+       <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+       <title>WEB - ${title}</title>
+       <link rel="stylesheet" href="style.css">
+       <script src="color.js"></script>
+     </head>
+     <body>
+       <div id="top">
+         <h1><a href="/">차</a></h1>
+         <input type="button" value="night" onclick="nightDayHandler(this)"/>
+       </div>
+       <div id="grid">
+        ${list}
+        <div id="article">
+          <h2>${title}</h2>
+          ${description}
+        </div>
+      </div>
+     </body>
+    </html>
+  `;
+}
+
 let app = http.createServer(function (request, response) {
   let _url = request.url;
   console.log(_url);
@@ -20,29 +49,7 @@ let app = http.createServer(function (request, response) {
 	      	i++;
 	      }
 	      list += '</ul>';
-        let template = `
-          <!DOCTYPE html>
-          <html lang="en">
-            <head>
-              <meta charset="UTF-8" />
-              <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-              <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-              <title>WEB - ${title}</title>
-              <link rel="stylesheet" href="style.css">
-              <script src="color.js"></script>
-            </head>
-            <body>
-              <div id="top">
-                <h1><a href="/">차</a></h1>
-                <input type="button" value="night" onclick="nightDayHandler(this)"/>
-              </div>
-              <div id="grid">
-                ${list}
-                <div id="article">${description}</div>
-              </div>
-            </body>
-          </html>
-        `;
+        template = templateHTML(title, list, description);
         response.writeHead(200);
         response.end(template);
       });
@@ -56,32 +63,10 @@ let app = http.createServer(function (request, response) {
 	      }
 	      list += '</ul>';
         fs.readFile(`data/${queryData.id}`, "utf-8", function (err, description) {
-         let title = queryData.id;
-          let template = `
-                <!DOCTYPE html>
-                <html lang="en">
-                  <head>
-                    <meta charset="UTF-8" />
-                    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                    <title>WEB - ${title}</title>
-                    <link rel="stylesheet" href="style.css">
-                   <script src="color.js"></script>
-                 </head>
-                 <body>
-                   <div id="top">
-                      <h1><a href="/">차</a></h1>
-                     <input type="button" value="night" onclick="nightDayHandler(this)"/>
-                   </div>
-                   <div id="grid">
-                     ${list}
-                     <div id="article">${description}</div>
-                   </div>
-                 </body>
-               </html>
-             `;
-         response.writeHead(200);
-         response.end(template);
+          let title = queryData.id;
+          template = templateHTML(title, list, description);
+          response.writeHead(200);
+          response.end(template);
         });
       });
     }
@@ -91,13 +76,11 @@ let app = http.createServer(function (request, response) {
       let fileContents = fs.readFileSync("style.css", "utf-8");
       response.write(fileContents);
       response.end();
-      // console.log("css 전송완료");
     } else if (_url == "/color.js") {
       response.writeHead(200, { "Content-type": "text/js" });
       let fileContents = fs.readFileSync("color.js", "utf-8");
       response.write(fileContents);
       response.end();
-      // console.log("js 전송완료");
     } else {
       response.writeHead(404);
       response.end("Not found");
