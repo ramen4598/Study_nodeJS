9ce0ecf3b3d75427a7db6752c7582145268f660c
tired_i <ramen4598@naver.com>
2023-02-10 23:54:30 +0900

    23.02.10 commit 1. use fs.readdir() to make file list. (파일 리스트 동적 생성)

--- main.js
+++ main.js
@@ -12,7 +12,15 @@ var app = http.createServer(function (request, response) {
     if (queryData.id === undefined) {
       var title = "Welcome";
       var description = "Hello Node.js";
-      var template = `
+      fs.readdir('./data', function(err, filelist){
+        let list = '<ul>';
+	      let i = 0; 
+        while(i < filelist.length){
+	      	list += `<li>${filelist[i]}</li>`;
+	      	i++;
+	      }
+	      list += '</ul>';
+        var template = `
           <!DOCTYPE html>
           <html lang="en">
             <head>
@@ -29,50 +37,52 @@ var app = http.createServer(function (request, response) {
                 <input type="button" value="night" onclick="nightDayHandler(this)"/>
               </div>
               <div id="grid">
-                <ul>
-                  <li><a href="/?id=black%20tea">홍차</a></li>
-                  <li><a href="/?id=oolang%20tea">우롱차</a></li>
-                  <li><a href="/?id=green%20tea">녹차</a></li>
-                </ul>
+                ${list}
                 <div id="article">${description}</div>
               </div>
             </body>
           </html>
         `;
-      response.writeHead(200);
-      response.end(template);
-    } else {
-      fs.readFile(`data/${queryData.id}`, "utf-8", function (err, description) {
-        var title = queryData.id;
-        var template = `
-              <!DOCTYPE html>
-              <html lang="en">
-                <head>
-                  <meta charset="UTF-8" />
-                  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-                  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                  <title>WEB - ${title}</title>
-                  <link rel="stylesheet" href="style.css">
-                  <script src="color.js"></script>
-                </head>
-                <body>
-                  <div id="top">
-                    <h1><a href="/">차</a></h1>
-                    <input type="button" value="night" onclick="nightDayHandler(this)"/>
-                  </div>
-                  <div id="grid">
-                    <ul>
-                      <li><a href="/?id=black%20tea">홍차</a></li>
-                      <li><a href="/?id=oolang%20tea">우롱차</a></li>
-                      <li><a href="/?id=green%20tea">녹차</a></li>
-                    </ul>
-                    <div id="article">${description}</div>
-                  </div>
-                </body>
-              </html>
-            `;
         response.writeHead(200);
         response.end(template);
+      });
+    } else {
+      fs.readdir('./data', function(err, filelist){
+        let list = '<ul>';
+	      let i = 0; 
+	      while(i < filelist.length){
+	      	list += `<li>${filelist[i]}</li>`;
+	      	i++;
+	      }
+	      list += '</ul>';
+        fs.readFile(`data/${queryData.id}`, "utf-8", function (err, description) {
+         var title = queryData.id;
+          var template = `
+                <!DOCTYPE html>
+                <html lang="en">
+                  <head>
+                    <meta charset="UTF-8" />
+                    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                    <title>WEB - ${title}</title>
+                    <link rel="stylesheet" href="style.css">
+                   <script src="color.js"></script>
+                 </head>
+                 <body>
+                   <div id="top">
+                      <h1><a href="/">차</a></h1>
+                     <input type="button" value="night" onclick="nightDayHandler(this)"/>
+                   </div>
+                   <div id="grid">
+                     ${list}
+                     <div id="article">${description}</div>
+                   </div>
+                 </body>
+               </html>
+             `;
+         response.writeHead(200);
+         response.end(template);
+        });
       });
     }
   } else {
