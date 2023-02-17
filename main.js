e576668a25d4c1d369cc9f2ed6b58df46a68d937
tired_i <ramen4598@naver.com>
2023-02-18 00:08:25 +0900

    make a module named 'template'

--- main.js
+++ main.js
@@ -2,46 +2,7 @@ const http = require("http");
 const fs = require("fs");
 const url = require("url");
 const qs = require("querystring");
-
-const template = {
-  HTML : function(title, list, control, description) {
-    return `
-     <!DOCTYPE html>
-     <html lang="en">
-       <head>
-         <meta charset="UTF-8" />
-         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-         <title>WEB - ${title}</title>
-         <link rel="stylesheet" href="style.css">
-         <script src="color.js"></script>
-       </head>
-       <body>
-         <div id="top">
-           <h1><a href="/">Board</a></h1>
-           <input type="button" value="night" onclick="nightDayHandler(this)"/>
-         </div>
-         <div id="grid">
-          ${list}
-          <div id="article">
-            ${control}
-            <h2>${title}</h2>
-            ${description}
-          </div>
-        </div>
-       </body>
-      </html>
-    `;
-  },
-  List : function(filelist) {
-    let list = "<ul>";
-    for (let i = 0; i < filelist.length; i++) {
-      list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-    }
-    list += "</ul>";
-    return list;
-  }
-}
+const template = require("./lib/template.js");
 
 const app = http.createServer(function (request, response) {
   const _url = request.url;
