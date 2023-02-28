2a5c17cf28dc2c1dc01ab62b38c23b037651a617
tired_i <ramen4598@naver.com>
2023-03-01 04:37:25 +0900

    study : asynchronous code test

---
+++ asyncFn.test.js
@@ -0,0 +1,37 @@
+const aFn = require('./asyncFn');
+
+test('어떻게 사람 이름이 ...', ()=>{
+	return aFn.getName().then((name)=>{
+		expect(name).toBe('엄준식');
+    },(reason)=>{
+        expect(reason).toBe('Error!');
+    });
+});
+
+test('어떻게 사람 이름이 ...', ()=>{
+	return expect(aFn.getName()).resolves.toBe('엄준식');
+});
+/*
+test('어떻게 사람 이름이 ...', ()=>{
+	return expect(aFn.getName()).rejects.toMatch('Error!');
+});
+*/
+
+test('어...어..어..ㅁ..', async()=> {
+	const data = await aFn.getUm();
+	expect(data).toBe('엄');
+	expect(data).not.toBe('준');
+	expect(data).not.toBe('식');
+});
+
+test('엄.준.', (done)=>{
+    function callback(Jun) {
+        try{
+            expect(Jun).toBe('e준');
+            done();
+        }catch(error){
+            done(error);
+        }
+    }
+    aFn.getJun(callback);
+});
