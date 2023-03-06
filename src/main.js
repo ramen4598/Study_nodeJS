7351f1ca36c396c149a9916518763cb87b83db75
tired_i <ramen4598@naver.com>
2023-03-07 01:06:25 +0900

    refactoring : readdir&&response(html) process

--- src/main.js
+++ src/main.js
@@ -8,21 +8,19 @@ const sanitizeHtml = require("sanitize-html");
 const template = require("./lib/template.js");
 const dataDir = "/app/src/data";
 
-
-
 const app = http.createServer(function (request, response) {
   const _url = request.url;
   const queryData = url.parse(_url, true).query;
   const pathname = url.parse(_url, true).pathname;
 
   /**디렉터리 안에 파일의 이름을 읽고  html response함.
-  * path, title, decscription, control
-  */
-  function readAndRes(path, title, description, control){
+   * path, title, decscription, control
+   */
+  function readAndRes(path, title, description, control) {
     fs.readdir(path, function (err, filelist) {
       const noData = `ENOENT: no such file or directory, scandir '/app/src/data'`;
-      if (err && err.message === noData){
-        fs.mkdirSync(path, {recursive: true});
+      if (err && err.message === noData) {
+        fs.mkdirSync(path, { recursive: true });
         filelist = [""];
       }
       let list = template.List(filelist);
@@ -41,32 +39,11 @@ const app = http.createServer(function (request, response) {
       `;
       readAndRes(dataDir, title, description, control);
     } else {
-      /*
-      fs.readdir(dataDir, function (err, filelist) {
-        let filteredTitle = path.parse(queryData.id).base;
-        fs.readFile(
-          `${dataDir}/${filteredTitle}`,
-          "utf-8",
-          function (err, description) {
-            let list = template.List(filelist);
-            let control = `
-              <input type="button" value="create" onclick="redirect(this, '${filteredTitle}')"/>
-              <input type="button" value="update" onclick="redirect(this, '${filteredTitle}')"/>
-              <form id="frm" action="delete_process" method="post" style="display:inline">
-                <input type="hidden" name="id" value="${filteredTitle}">
-                <input type="button" value="delete" 
-                onclick="if(confirm('really delete?')==true){document.getElementById('frm').submit();}">
-              </form>
-            `;
-            let html = template.HTML(filteredTitle, list, control, description);
-            response.writeHead(200);
-            response.end(html);
-          }
-        );
-      });
-      */
       const filteredTitle = path.parse(queryData.id).base;
-      const description = fs.readFileSync(`${dataDir}/${filteredTitle}`, "utf-8");
+      const description = fs.readFileSync(
+        `${dataDir}/${filteredTitle}`,
+        "utf-8"
+      );
       let control = `
         <input type="button" value="create" onclick="redirect(this, '${filteredTitle}')"/>
         <input type="button" value="update" onclick="redirect(this, '${filteredTitle}')"/>
@@ -79,9 +56,8 @@ const app = http.createServer(function (request, response) {
       readAndRes(dataDir, filteredTitle, description, control);
     }
   } else if (pathname === "/create") {
-    fs.readdir(dataDir, function (err, filelist) {
-      let title = "create";
-      let description = `
+    let title = "create";
+    let description = `
         <form action="/create_process" method="post">
 	        <p><input type="text" name="title" placeholder="title"></p>
 	        <p>
@@ -92,12 +68,8 @@ const app = http.createServer(function (request, response) {
 	        </p>
         </form>
       `;
-      let list = template.List(filelist);
-      let control = ``;
-      let html = template.HTML(title, list, control, description);
-      response.writeHead(200);
-      response.end(html);
-    });
+    let control = ``;
+    readAndRes(dataDir, title, description, control);
   } else if (pathname === "/create_process") {
     let body = "";
     request.on("data", function (data) {
@@ -110,36 +82,36 @@ const app = http.createServer(function (request, response) {
       let filteredTitle = path.parse(title).base;
       let sanitizedTitle = sanitizeHtml(filteredTitle);
       let sanitizedDesc = sanitizeHtml(description);
-      fs.writeFile(`${dataDir}/${sanitizedTitle}`, sanitizedDesc, "utf8", function (err) {
-        response.writeHead(302, { Location: encodeURI(`/?id=${sanitizedTitle}`)});
-        response.end();
-      });
+      fs.writeFile(
+        `${dataDir}/${sanitizedTitle}`,
+        sanitizedDesc,
+        "utf8",
+        function (err) {
+          response.writeHead(302, {
+            Location: encodeURI(`/?id=${sanitizedTitle}`),
+          });
+          response.end();
+        }
+      );
     });
   } else if (pathname === "/update") {
-    let filteredTitle = path.parse(queryData.id).base;
-    fs.readFile(`${dataDir}/${filteredTitle}`, "utf8", function (err, description) {
-      fs.readdir(dataDir, function (err, filelist) {
-        let updateForm = `
-          <form action="/update_process" method="post">
-	          <p>
-              <input type="hidden" name="id" value="${filteredTitle}">
-              <input type="text" name="title" placeholder="title" value="${filteredTitle}">
-            </p>
-	          <p>
-	           	<textarea name="description" placeholder="description">${description}</textarea>
-	          </p>
-	          <p>
-	           	<input type="submit">
-	          </p>
-          </form>
-        `;
-        let list = template.List(filelist);
-        let control = ``;
-        let html = template.HTML(filteredTitle, list, control, updateForm);
-        response.writeHead(200);
-        response.end(html);
-      });
-    });
+    const filteredTitle = path.parse(queryData.id).base;
+    const description = fs.readFileSync(`${dataDir}/${filteredTitle}`, "utf8");
+    const updateForm = `
+      <form action="/update_process" method="post">
+          <p>
+          <input type="hidden" name="id" value="${filteredTitle}">
+          <input type="text" name="title" placeholder="title" value="${filteredTitle}"> </p>
+          <p>
+               <textarea name="description" placeholder="description">${description}</textarea>
+          </p>
+          <p>
+               <input type="submit">
+          </p>
+      </form>
+    `;
+    let control = ``;
+    readAndRes(dataDir, filteredTitle, updateForm, control);
   } else if (pathname === "/update_process") {
     let body = "";
     request.on("data", function (data) {
@@ -155,19 +127,30 @@ const app = http.createServer(function (request, response) {
       let sanitizedId = sanitizeHtml(filteredId);
       let sanitizedTitle = sanitizeHtml(filteredTitle);
       let sanitizedDesc = sanitizeHtml(description);
-      fs.rename(`${dataDir}/${sanitizedId}`, `./data/${sanitizedTitle}`, function (err) {
-        fs.writeFile(`${dataDir}/${sanitizedTitle}`, sanitizedDesc, "utf8", function (err) {
-          response.writeHead(302, { Location: encodeURI(`/?id=${sanitizedTitle}`)});
-          response.end();
-        });
-      });
+      fs.rename(
+        `${dataDir}/${sanitizedId}`,
+        `./data/${sanitizedTitle}`,
+        function (err) {
+          fs.writeFile(
+            `${dataDir}/${sanitizedTitle}`,
+            sanitizedDesc,
+            "utf8",
+            function (err) {
+              response.writeHead(302, {
+                Location: encodeURI(`/?id=${sanitizedTitle}`),
+              });
+              response.end();
+            }
+          );
+        }
+      );
     });
   } else if (pathname === "/delete_process") {
     let body = "";
     request.on("data", function (data) {
       body += data;
     });
-    request.on("end", function (){
+    request.on("end", function () {
       let post = qs.parse(body);
       let id = post.id;
       let filteredId = path.parse(id).base;
@@ -177,21 +160,23 @@ const app = http.createServer(function (request, response) {
       });
     });
   } else {
-  	function resFile (path){
-		response.writeHead(200, { "Content-type": "text/css" });
-		let fileContents = fs.readFileSync(path, "utf-8");
-		response.write(fileContents);
-		response.end();
-	}
+    function resFile(path) {
+      let fileContents = fs.readFileSync(path, "utf-8");
+      response.write(fileContents);
+      response.end();
+    }
     if (_url == "/style.css") {
-		const path = "/app/src/style.css";
-		resFile(path);
+      response.writeHead(200, { "Content-type": "text/css" });
+      const path = "/app/src/style.css";
+      resFile(path);
     } else if (_url == "/lib/color.js") {
-    	const path = "/app/src/lib/color.js"
-		resFile(path);
+      response.writeHead(200, { "Content-type": "text/js" });
+      const path = "/app/src/lib/color.js";
+      resFile(path);
     } else if (_url == "/lib/crudBtn.js") {
-    	const path = "/app/src/lib/crudBtn.js";
-		resFile(path);
+      response.writeHead(200, { "Content-type": "text/js" });
+      const path = "/app/src/lib/crudBtn.js";
+      resFile(path);
     } else {
       response.writeHead(404);
       response.end("Not found");
