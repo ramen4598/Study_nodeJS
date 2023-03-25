74b5f39488a9e14b74bf987aeb9d15e443da22e5
tired_i <ramen4598@naver.com>
2023-03-25 23:48:35 +0900

    MySQL로 update 구현

--- src/main.js
+++ src/main.js
@@ -7,13 +7,13 @@ const sanitizeHtml = require("sanitize-html");
 
 const template = require("./lib/template.js");
 const dataDir = "/app/src/data";
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 const db = mysql.createConnection({
-  host: process.env.MYSQL_HOST, 
-  user: process.env.MYSQL_USER, 
-  password: process.env.MYSQL_PASSWORD, 
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
-  port: process.env.MYSQL_PORT
+  port: process.env.MYSQL_PORT,
 });
 db.connect();
 
@@ -22,21 +22,20 @@ const app = http.createServer(function (request, response) {
   const queryData = url.parse(_url, true).query;
   const pathname = url.parse(_url, true).pathname;
 
-
   /**디렉터리 안에 파일의 이름을 읽고  html response함.
    * path, title, decscription, control
    */
-	function readAndRes(path, title, description, control) {
-		db.query(`SELECT * FROM topic`, function(error, topics){
-    	if(error){
-        	throw error;
-      	}
-		let list = template.List(topics);
-		let html = template.HTML(title, list, control, description);
-		response.writeHead(200);
-		response.end(html);
-		});
-	}
+  function readAndRes(path, title, description, control) {
+    db.query(`SELECT * FROM topic`, function (error, topics) {
+      if (error) {
+        throw error;
+      }
+      let list = template.List(topics);
+      let html = template.HTML(title, list, control, description);
+      response.writeHead(200);
+      response.end(html);
+    });
+  }
 
   if (pathname === "/") {
     if (queryData.id === undefined) {
@@ -47,24 +46,28 @@ const app = http.createServer(function (request, response) {
       `;
       readAndRes(dataDir, title, description, control);
     } else {
-		  db.query(`SELECT * FROM topic WHERE id=?`, [queryData.id], function(error, topic){
-    	  if(error){
-        	throw error;
-      	}
-        const title = topic[0].title;
-        const description = topic[0].description;
-        const filteredId = path.parse(queryData.id).base;
-        let control = `
-          <input type="button" value="create" onclick="redirect(this, '${filteredId}')"/>
-          <input type="button" value="update" onclick="redirect(this, '${filteredId}')"/>
+      db.query(
+        `SELECT * FROM topic WHERE id=?`,
+        [queryData.id],
+        function (error, topic) {
+          if (error) {
+            throw error;
+          }
+          const title = topic[0].title;
+          const description = topic[0].description;
+          const id = queryData.id;
+          let control = `
+          <input type="button" value="create" onclick="redirect(this, '${id}')"/>
+          <input type="button" value="update" onclick="redirect(this, '${id}')"/>
           <form id="frm" action="delete_process" method="post" style="display:inline">
-            <input type="hidden" name="id" value="${filteredId}">
+            <input type="hidden" name="id" value="${id}">
             <input type="button" value="delete" 
             onclick="if(confirm('really delete?')==true){document.getElementById('frm').submit();}">
           </form>
         `;
-        readAndRes(dataDir, title, description, control);
-      });
+          readAndRes(dataDir, title, description, control);
+        }
+      );
     }
   } else if (pathname === "/create") {
     let title = "create";
@@ -92,10 +95,12 @@ const app = http.createServer(function (request, response) {
       let description = post.description;
       let sanitizedTitle = sanitizeHtml(title);
       let sanitizedDesc = sanitizeHtml(description);
-      db.query(`INSERT INTO topic (title, description, created, author_id) 
-        VALUES (?, ?, NOW(), ?)`,[sanitizedTitle, sanitizedDesc, 1],
-        function(error, result){
-          if(error){
+      db.query(
+        `INSERT INTO topic (title, description, created, author_id) 
+        VALUES (?, ?, NOW(), ?)`,
+        [sanitizedTitle, sanitizedDesc, 1],
+        function (error, result) {
+          if (error) {
             throw error;
           }
           response.writeHead(302, {
@@ -106,23 +111,33 @@ const app = http.createServer(function (request, response) {
       );
     });
   } else if (pathname === "/update") {
-    const filteredTitle = path.parse(queryData.id).base;
-    const description = fs.readFileSync(`${dataDir}/${filteredTitle}`, "utf8");
-    const updateForm = `
-      <form action="/update_process" method="post">
-          <p>
-          <input type="hidden" name="id" value="${filteredTitle}">
-          <input type="text" name="title" placeholder="title" value="${filteredTitle}"> </p>
-          <p>
-               <textarea name="description" placeholder="description">${description}</textarea>
-          </p>
-          <p>
-               <input type="submit">
-          </p>
-      </form>
-    `;
-    let control = ``;
-    readAndRes(dataDir, filteredTitle, updateForm, control);
+    db.query(
+      `SELECT * FROM topic WHERE id=?`,
+      [queryData.id],
+      function (error, topic) {
+        if (error) {
+          throw error;
+        }
+        const title = topic[0].title;
+        const description = topic[0].description;
+        const id = queryData.id;
+        let control = ``;
+        const updateForm = `
+          <form action="/update_process" method="post">
+              <p>
+              <input type="hidden" name="id" value="${id}">
+              <input type="text" name="title" placeholder="title" value="${title}"> </p>
+              <p>
+                   <textarea name="description" placeholder="description">${description}</textarea>
+              </p>
+              <p>
+                   <input type="submit">
+              </p>
+          </form>
+        `;
+        readAndRes(dataDir, title, updateForm, control);
+      }
+    );
   } else if (pathname === "/update_process") {
     let body = "";
     request.on("data", function (data) {
